3275bef3138cd1a9df89c03c50614b40e8c1f736
Shacklebolt13 <19cse223.gagandeepsinghbhambrah@giet.edu>
2022-09-23 23:08:19 +0530

    removed navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
@@ -12,11 +11,11 @@ import { MatToolbarModule} from "@angular/material/toolbar"
 import { MatIconModule } from "@angular/material/icon"
 import {MatListModule} from "@angular/material/list";
 import { PostComponent } from './post/post.component'
+import {MatTabsModule} from '@angular/material/tabs';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
     PostComponent
   ],
   imports: [
@@ -27,6 +26,7 @@ import { PostComponent } from './post/post.component'
     MatToolbarModule,
     MatIconModule,
     MatListModule,
+    MatTabsModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the application is stable
