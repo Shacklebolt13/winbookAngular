656f6125c40bbcaaa72cc4f56561759e255a2ae1
Shacklebolt13 <19cse223.gagandeepsinghbhambrah@giet.edu>
2022-09-23 14:54:12 +0530

    added navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,26 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { MatSidenavModule} from "@angular/material/sidenav"
+import { MatToolbarModule} from "@angular/material/toolbar"
+import { MatIconModule } from "@angular/material/icon"
+import {MatListModule} from "@angular/material/list";
+import { PostComponent } from './post/post.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the application is stable
